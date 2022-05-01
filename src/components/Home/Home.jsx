1ebe8e2cb7531736e846fbfa6b4c07687d3c3b4a
Cassas Tomás <tomascassas@gmail.com>
2022-05-01 19:58:04 -0300

    Cart auto save in localStorage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,7 +5,7 @@ import { motion,  } from 'framer-motion'
 
 const Home = () => {
     return (
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} exit={{ opacity: 0 }}>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, transition: {delay: 0.7} }} exit={{ opacity: 0 }}>
             <HomeWrapper>
                 <HighlightHeader>
                     <BrandHighlight>
